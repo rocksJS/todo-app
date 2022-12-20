86911a67d099f73b76cfb1d660fea6a6982ad9ed
rocksJS <6abyghost@gmail.com>
2022-12-20 04:55:31 +0300

    commit before install ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing,module';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [AppRoutingModule, BrowserModule, CommonModule, MatSelectModule, BrowserAnimationsModule],
